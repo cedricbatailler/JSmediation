c7fcddac26d2811519830c699c74b761bcc415e7
Cedric Batailler <cedric.batailler@gmail.com>
2021-06-17 15:53:39 +0200

    change related to testthat 3ed

--- tests/testthat/test-mdt_moderated.R
+++ tests/testthat/test-mdt_moderated.R
@@ -18,7 +18,7 @@ test_that("used models are correctly specified", {
   
   models_js <- purrr::pluck(js_model, "js_models")
   
-  expect_equivalent(models_js, models)
+  expect_equal(models_js, models, ignore_attr = TRUE)
 })
 
 test_that("mdt_moderated does not throw error", {
