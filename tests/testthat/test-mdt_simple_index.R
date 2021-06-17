e901ec143b9ef2e9a15a809f1df4ceeba88d88d0
Cedric Batailler <cedric.batailler@gmail.com>
2021-06-17 11:24:48 +0200

    Add new tests to check the CI add_index produce (mdt_simple)

--- tests/testthat/test-mdt_simple_index.R
+++ tests/testthat/test-mdt_simple_index.R
@@ -3,7 +3,7 @@ test_that("add_index method does not throw error", {
   ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
                                          "Low discrimination",
                                          "High discrimination")
-  
+
   simple_mediation <- 
     mdt_simple(data = ho_et_al,
                IV   = condition_c,
@@ -12,3 +12,29 @@ test_that("add_index method does not throw error", {
   
   expect_silent(add_index(simple_mediation))
 })
+
+test_that("simple mediation index produce the intended intervals", {
+  withr::with_seed(123, 
+  {
+    simple_model <- 
+      ho_et_al %>% 
+      dplyr::mutate(condition_c = build_contrast(condition,
+                                                 "Low discrimination",
+                                                 "High discrimination")) %>% 
+      mdt_simple(condition_c,
+                 hypodescent, 
+                 linkedfate)
+
+    model_index <-
+      add_index(simple_model, times = 5e6) %>% 
+      purrr::pluck("indirect_index_infos", "CI")
+
+    # CIs produce the right values
+    expect_equal(model_index[[1]], 0.0894, 
+                 tolerance = 1e-3)
+    expect_equal(model_index[[2]], 0.2060, 
+                 tolerance = 1e-3)
+
+  })
+})
+
