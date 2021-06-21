c80a23a65fb3ffff880da98e947364dfb6fc5df9
Cedric Batailler <cedric.batailler@gmail.com>
2021-06-21 15:04:20 +0200

    `check_assumptions` test each models

--- tests/testthat/test-check-assumptions.R
+++ tests/testthat/test-check-assumptions.R
@@ -13,3 +13,22 @@ test_that("check_assumptions run without error", {
     expect_error(check_assumptions(my_model), NA)
 
 })
+
+test_that("check_assumptions test assumptions for each models", {
+  
+  ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
+                                         "Low discrimination",
+                                         "High discrimination")
+  
+  my_model <-
+    mdt_simple(data = ho_et_al,
+               IV = condition_c,
+               DV = hypodescent,
+               M = linkedfate)
+
+  purrr::pluck(my_model, "js_models") %>%
+    names() %>%
+    stringr::str_replace("\\+", "\\\\+") %>% 
+    purrr::map(~expect_output(check_assumptions(my_model), .x))
+  
+})
