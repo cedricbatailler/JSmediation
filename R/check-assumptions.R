2c16d2ae98f74e8cda890da107fdc3110eb73b0d
Cedric Batailler <cedric.batailler@gmail.com>
2021-06-25 14:38:35 +0200

    style

--- R/check-assumptions.R
+++ R/check-assumptions.R
@@ -1,15 +1,14 @@
-#' @title Test assumptions for models underlying the mediation 
+#' @title Test assumptions for models underlying the mediation
 #'
 #' @description When conducting a joint-significant test, different models are
 #'   fitted to the data. This function tests the normality and
 #'   heteroscedasticity assumptions for these models using \code{performance}.
 #'   \code{check_assumptions} uses \code{\link[performance]{check_normality}}
 #'   and \code{\link[performance]{check_heteroscedasticity}} to test the
-#'   assumptions. 
-#'   
+#'   assumptions.
+#'
 #'   Note that \code{check_assumptions} returns a
 #'   \code{mediation_model} object.
-#'
 #'
 #' @param mediation_model An object of class \code{mediation_model}.
 #'
@@ -29,30 +28,31 @@
 #' check_assumptions(my_model)
 #'
 #' @export
-check_assumptions <- function(mediation_model, 
+check_assumptions <- function(mediation_model,
                               tests = c("normality", "heteroscedasticity")) {
   UseMethod("check_assumptions")
 }
 
 #' @export
-check_assumptions.mediation_model <- 
-  function(mediation_model, 
+check_assumptions.mediation_model <-
+  function(mediation_model,
            tests = c("normality", "heteroscedasticity")) {
-    
+
     rlang::check_installed("performance")
-    
+
     supported_tests <- c("normality", "heteroscedasticity", "outliers")
-    
-    if(length(tests) < 1L) {
+
+    if (length(tests) < 1L) {
       rlang::abort(c("`tests` argument must contains at least one element."))
     }
-    
-    if(! is.character(tests)) {
+
+    if (! is.character(tests)) {
       rlang::abort(c("`tests` argument must be a character vector."))
     }
-    
-    if(sum(! tests %in% supported_tests) >= 1) {
-      rlang::warn(message = 
+
+    if (sum(! tests %in% supported_tests) >= 1) {
+      
+      rlang::warn(message =
                     c("`tests` argument in `check_assumptions` contains unsupported checks.",
                       i  = "Supported checks are:",
                       "- normality",
@@ -61,9 +61,9 @@ check_assumptions.mediation_model <-
       )
     }
 
-    purrr::pluck(mediation_model, "js_models") %>% 
+    purrr::pluck(mediation_model, "js_models") %>%
       purrr::imap(~ check_model(.y, .x, tests))
-    
+
     invisible(mediation_model)
   }
 
@@ -75,9 +75,13 @@ check_assumptions.mediation_model <-
 
 check_model <- function(model_name, model, tests) {
   cat(model_name, sep = "\n")
-  
-  if("normality" %in% tests)          { performance::check_normality(model) }
-  if("heteroscedasticity" %in% tests) { performance::check_heteroscedasticity(model) }
-  if("outliers" %in% tests)           { performance::check_outliers(model) }
-  
+  if ("normality" %in% tests) {
+    performance::check_normality(model)
+    }
+  if ("heteroscedasticity" %in% tests) {
+    performance::check_heteroscedasticity(model)
+    }
+  if ("outliers" %in% tests) {
+    performance::check_outliers(model)
+    }
 }
