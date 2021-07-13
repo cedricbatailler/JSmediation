589717dcbc0707d32d663a0ea42efeae2d76162a
Cedric Batailler <cedric.batailler@gmail.com>
2021-07-13 10:49:14 +0200

    Add type check

--- tests/testthat/test-mdt_within.R
+++ tests/testthat/test-mdt_within.R
@@ -28,3 +28,32 @@ test_that("print method for mdt_within does not throw error", {
   expect_output(print(model))
   expect_output(print(add_index(model)))
 })
+
+test_that("Type checks work as intended in mdt_within", {
+  data(dohle_siegrist)
+  dohle_siegrist$foo <- "foo"
+
+  expect_error(
+    mdt_within(dohle_siegrist,
+               foo,
+               willingness,
+               hazardousness,
+               participant)
+  )
+
+  expect_error( 
+    mdt_within(dohle_siegrist,
+               name,
+               foo,
+               hazardousness,
+               participant)
+  )
+
+  expect_error(
+    mdt_within(dohle_siegrist,
+               name,
+               willingness,
+               foo,
+               participant)
+  )
+})
