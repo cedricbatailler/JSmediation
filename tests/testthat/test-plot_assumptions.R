e81ecef266d20d3b8fe38b2ec6020f6e2140c114
Cedric Batailler <cedric.batailler@gmail.com>
2021-08-31 13:57:36 +0200

    use vdiffr

--- tests/testthat/test-plot_assumptions.R
+++ tests/testthat/test-plot_assumptions.R
@@ -1,36 +1,27 @@
 test_that("check_assumptions_plot", {
-  # set seed
-  withr::local_seed(123)
-
   # run mediation model
   data(ho_et_al)
   ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
                                          "Low discrimination",
                                          "High discrimination")
-  my_model <-
+  mediation_fit <-
     mdt_simple(data = ho_et_al,
                IV = condition_c,
                DV = hypodescent,
                M = linkedfate)
 
-  # number of models to test
-  number_of_models <- purrr::chuck(my_model, "js_models") %>% length()
+  plot_assumptions_without_warn <- function(x, seed = 123) {
+    withr::local_options(list(warn = -1))
+    withr::local_seed(seed)
 
-  # prepare plotting
-  plot_tempdir <- tempdir()
-  plot_path <- glue::glue("{plot_tempdir}/assumption-plot-%02d.png")
+    plot_assumptions(x)
+  }
 
-  # save plots in tempdir -----------------------------------------------------
-  # use warn=-1 because performance::check_models sometimes generates error as
-  # a normal behavior
-  png(plot_path)
-  withr::with_options(list(warn = -1), {
-    plot_assumptions(my_model)
-    })
-  dev.off()
-
-  # compare the models --------------------------------------------------------
-  sprintf(plot_path, 1:number_of_models) %>%
-    purrr::map(~ expect_snapshot_file(.x, cran = FALSE))
+  # HELP WANTED:
+  # Uses vdiffr to check the result of `plot_assumptions`. Because
+  # `plot_assumptions` produces three plot automatically and because vdiffr
+  # automatically captures the last plot, the only plot testes is the last one.
+  vdiffr::expect_doppelganger(title = "Last plot plot_assumptions produces",
+                              fig = plot_assumptions_without_warn(mediation_fit))
 
 })
